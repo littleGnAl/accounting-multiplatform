c3ac5e1f8d86d3e1d7a81c80c86373654d0a14ef
littleGnAl <tearjeaker@hotmail.com>
2019-06-08 15:06:15 +0800

    Update AGP -> 5.4.1, kotlin -> 1.3.31, migrate to Androidx

--- android/app/src/main/kotlin/com/littlegnal/accountingmultiplatform/MainActivity.kt
+++ android/app/src/main/kotlin/com/littlegnal/accountingmultiplatform/MainActivity.kt
@@ -3,9 +3,10 @@ package com.littlegnal.accountingmultiplatform
 import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
+import io.flutter.app.FlutterFragmentActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
