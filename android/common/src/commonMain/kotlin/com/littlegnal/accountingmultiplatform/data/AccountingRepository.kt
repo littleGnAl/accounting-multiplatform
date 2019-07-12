c5d3e1a15fe1ed749a4adafa5d6b1e1db6414e3b
Littlegnal <8847263+littleGnAl@users.noreply.github.com>
2019-07-12 17:11:37 +0800

    Upgrade Flutter 1.7, Kotlin 1.3.41 (#8)
    * Upgrade Flutter 1.7, Kotlin 1.3.41

--- android/common/src/commonMain/kotlin/com/littlegnal/accountingmultiplatform/data/AccountingRepository.kt
+++ android/common/src/commonMain/kotlin/com/littlegnal/accountingmultiplatform/data/AccountingRepository.kt
@@ -1,6 +1,10 @@
 package com.littlegnal.accountingmultiplatform.data
 
 import com.littlegnal.accountingmultiplatform.AccountingDB
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
 import kotlinx.serialization.list
@@ -11,76 +15,96 @@ class AccountingRepository(private val accountingDB: AccountingDB) {
     Json(JsonConfiguration.Stable)
   }
 
-  fun queryPreviousAccounting(lastDateTimeMilliseconds: Long, limit: Long): String {
-    val list = accountingDB.accountingDBQueries
-        .queryPreviousAccounting(lastDateTimeMilliseconds, limit)
-        .executeAsList()
-        .map {
-          it.toAccountingSerialization()
-        }
+  suspend fun queryPreviousAccountingAsync(
+    lastDateTimeMilliseconds: Long,
+    limit: Long
+  ): Deferred<String> = coroutineScope {
+      async(runCoroutineDispatcher) {
+        val list = accountingDB.accountingDBQueries
+            .queryPreviousAccounting(lastDateTimeMilliseconds, limit)
+            .executeAsList()
+            .map {
+              it.toAccountingSerialization()
+            }
 
-    return json.stringify(AccountingSerialization.serializer().list, list)
-  }
+        json.stringify(AccountingSerialization.serializer().list, list)
+      }
+    }
 
-  fun insertAccounting(
+  suspend fun insertAccounting(
     id: Long,
     amount: Double,
     createTime: Long,
     tagName: String,
     remarks: String?
-  ) {
-    accountingDB.accountingDBQueries.insertAccounting(
-        id = id,
-        amount = amount,
-        createTime = createTime,
-        tag_name = tagName,
-        remarks = remarks)
+  ) = coroutineScope {
+    launch(runCoroutineDispatcher) {
+      accountingDB.accountingDBQueries.insertAccounting(
+          id = id,
+          amount = amount,
+          createTime = createTime,
+          tag_name = tagName,
+          remarks = remarks)
+    }
   }
 
-  fun deleteAccountingById(id: Long) {
-    accountingDB.accountingDBQueries.deleteAccountingById(id)
+  suspend fun deleteAccountingById(id: Long) = coroutineScope {
+    launch(runCoroutineDispatcher) {
+      accountingDB.accountingDBQueries.deleteAccountingById(id)
+    }
   }
 
-  fun getAccountingById(id: Long): String {
-    val r = accountingDB.accountingDBQueries.getAccountingById(id)
-        .executeAsOne()
-        .toAccountingSerialization()
-    return json.stringify(AccountingSerialization.serializer(), r)
+  suspend fun getAccountingByIdAsync(id: Long): Deferred<String> = coroutineScope {
+    async(runCoroutineDispatcher) {
+      val r = accountingDB.accountingDBQueries.getAccountingById(id)
+          .executeAsOne()
+          .toAccountingSerialization()
+      json.stringify(AccountingSerialization.serializer(), r)
+    }
   }
 
-  fun totalExpensesOfDay(timeMilliseconds: Long): Double {
-    return accountingDB.accountingDBQueries
-        .totalExpensesOfDay(timeMilliseconds, timeMilliseconds)
-        .executeAsOne()
-  }
+  suspend fun totalExpensesOfDayAsync(timeMilliseconds: Long): Deferred<Double> =
+    coroutineScope {
+      async(runCoroutineDispatcher) {
+        accountingDB.accountingDBQueries
+            .totalExpensesOfDay(timeMilliseconds, timeMilliseconds)
+            .executeAsOne()
+      }
+    }
 
-  fun getMonthTotalAmount(yearAndMonthList: List<String>): String {
-    val list = mutableListOf<GetMonthTotalAmount>()
-        .apply {
-          for (yearAndMonth in yearAndMonthList) {
-            val r = accountingDB.accountingDBQueries
-                .getMonthTotalAmount(yearAndMonth)
-                .executeAsOneOrNull()
+  suspend fun getMonthTotalAmountAsync(yearAndMonthList: List<String>): Deferred<String> =
+    coroutineScope {
+      async(runCoroutineDispatcher) {
+        val list = mutableListOf<GetMonthTotalAmount>()
+            .apply {
+              for (yearAndMonth in yearAndMonthList) {
+                val r = accountingDB.accountingDBQueries
+                    .getMonthTotalAmount(yearAndMonth)
+                    .executeAsOneOrNull()
 
-            if (r?.total != null && r.yearMonth != null) {
-              add(r)
+                if (r?.total != null && r.yearMonth != null) {
+                  add(r)
+                }
+              }
             }
-          }
-        }
-        .map {
-          it.toGetMonthTotalAmountSerialization()
-        }
+            .map {
+              it.toGetMonthTotalAmountSerialization()
+            }
 
-    return json.stringify(GetMonthTotalAmountSerialization.serializer().list, list)
-  }
+        json.stringify(GetMonthTotalAmountSerialization.serializer().list, list)
+      }
+    }
   
-  fun getGroupingMonthTotalAmount(yearAndMonth: String): String {
-    val list = accountingDB.accountingDBQueries
-        .getGroupingMonthTotalAmount(yearAndMonth)
-        .executeAsList()
-        .map {
-          it.toGetGroupingMonthTotalAmountSerialization()
-        }
-    return json.stringify(GetGroupingMonthTotalAmountSerialization.serializer().list, list)
-  }
+  suspend fun getGroupingMonthTotalAmountAsync(yearAndMonth: String): Deferred<String> =
+    coroutineScope {
+      async(runCoroutineDispatcher) {
+        val list = accountingDB.accountingDBQueries
+            .getGroupingMonthTotalAmount(yearAndMonth)
+            .executeAsList()
+            .map {
+              it.toGetGroupingMonthTotalAmountSerialization()
+            }
+        json.stringify(GetGroupingMonthTotalAmountSerialization.serializer().list, list)
+      }
+    }
 }
--- android/common/src/iosMain/kotlin/com/littlegnal/accountingmultiplatform/data/CoroutineDispatcher.kt
+++ android/common/src/iosMain/kotlin/com/littlegnal/accountingmultiplatform/data/CoroutineDispatcher.kt
@@ -1,23 +1,22 @@
 package com.littlegnal.accountingmultiplatform.data
 
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Runnable
 import platform.darwin.dispatch_async
 import platform.darwin.dispatch_get_main_queue
-import platform.darwin.dispatch_queue_t
 import kotlin.coroutines.CoroutineContext
 
-actual val runCoroutineDispatcher: CoroutineContext = NsQueueDispatcher(dispatch_get_main_queue())
+actual val runCoroutineDispatcher: CoroutineContext = NsQueueDispatcher
 
-class NsQueueDispatcher(private val dispatchQueue: dispatch_queue_t) : CoroutineDispatcher() {
+actual val uiCoroutineDispatcher: CoroutineContext = NsQueueDispatcher
+
+object NsQueueDispatcher : CoroutineDispatcher() {
   override fun dispatch(
     context: CoroutineContext,
     block: Runnable
   ) {
-    dispatch_async(dispatchQueue) {
+    dispatch_async(dispatch_get_main_queue()) {
       block.run()
     }
   }
-
 }
