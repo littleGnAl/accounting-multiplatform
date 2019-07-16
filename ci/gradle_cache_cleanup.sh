83c6b306293d00e92d0d79eddacf5245acce2eaa
littleGnAl <tearjeaker@hotmail.com>
2019-07-16 15:52:44 +0800

    Cirrus ci gradle cache

--- ci/gradle_cache_cleanup.sh
+++ ci/gradle_cache_cleanup.sh
@@ -3,8 +3,8 @@ cd android
 gradle_wrapper_version=$(./gradlew -q getGradleWrapperVersion | tail -n 1)
 echo "gradle wrapper version: $gradle_wrapper_version"
 
-- rm -rf ~/.gradle/caches/${gradle_wrapper_version}/
-- rm -rf ~/.gradle/caches/transforms-1
-- rm -rf ~/.gradle/caches/transforms-2
-- rm -rf ~/.gradle/caches/journal-1
-- find ~/.gradle/caches/ -name "*.lock" -type f -delete
+rm -rf ~/.gradle/caches/${gradle_wrapper_version}/
+rm -rf ~/.gradle/caches/transforms-1
+rm -rf ~/.gradle/caches/transforms-2
+rm -rf ~/.gradle/caches/journal-1
+find ~/.gradle/caches/ -name "*.lock" -type f -delete
