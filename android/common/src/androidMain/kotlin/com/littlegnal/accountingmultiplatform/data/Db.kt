b589f2f6d395b7fa09dccf1aac3e4359008440e7
littleGnAl <tearjeaker@hotmail.com>
2019-07-23 17:59:20 +0800

    Add kotlin code style & copyright

--- android/common/src/androidMain/kotlin/com/littlegnal/accountingmultiplatform/data/Db.kt
+++ android/common/src/androidMain/kotlin/com/littlegnal/accountingmultiplatform/data/Db.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2019 littlegnal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.littlegnal.accountingmultiplatform.data
 
 import com.littlegnal.accountingmultiplatform.AccountingDB
@@ -6,8 +22,6 @@ import com.squareup.sqldelight.db.SqlDriver
 object Db {
   private var driverRef: SqlDriver? = null
   private var dbRef: AccountingDB? = null
-
-
 
   val ready: Boolean
     get() = driverRef != null
--- android/common/src/commonMain/kotlin/com/littlegnal/accountingmultiplatform/data/AccountingRepository.kt
+++ android/common/src/commonMain/kotlin/com/littlegnal/accountingmultiplatform/data/AccountingRepository.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2019 littlegnal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.littlegnal.accountingmultiplatform.data
 
 import com.littlegnal.accountingmultiplatform.AccountingDB
@@ -72,39 +88,41 @@ class AccountingRepository(private val accountingDB: AccountingDB) {
       }
     }
 
-  suspend fun getMonthTotalAmountAsync(yearAndMonthList: List<String>): Deferred<String> =
-    coroutineScope {
-      async(runCoroutineDispatcher) {
-        val list = mutableListOf<GetMonthTotalAmount>()
-            .apply {
-              for (yearAndMonth in yearAndMonthList) {
-                val r = accountingDB.accountingDBQueries
-                    .getMonthTotalAmount(yearAndMonth)
-                    .executeAsOneOrNull()
+  suspend fun getMonthTotalAmountAsync(
+    yearAndMonthList: List<String>
+  ): Deferred<String> = coroutineScope {
+    return@coroutineScope async(runCoroutineDispatcher) {
+      val list = mutableListOf<GetMonthTotalAmount>()
+          .apply {
+            for (yearAndMonth in yearAndMonthList) {
+              val r = accountingDB.accountingDBQueries
+                  .getMonthTotalAmount(yearAndMonth)
+                  .executeAsOneOrNull()
 
-                if (r?.total != null && r.yearMonth != null) {
-                  add(r)
-                }
+              if (r?.total != null && r.yearMonth != null) {
+                add(r)
               }
             }
-            .map {
-              it.toGetMonthTotalAmountSerialization()
-            }
+          }
+          .map {
+            it.toGetMonthTotalAmountSerialization()
+          }
 
-        json.stringify(GetMonthTotalAmountSerialization.serializer().list, list)
-      }
+      return@async json.stringify(GetMonthTotalAmountSerialization.serializer().list, list)
     }
-  
-  suspend fun getGroupingMonthTotalAmountAsync(yearAndMonth: String): Deferred<String> =
-    coroutineScope {
-      async(runCoroutineDispatcher) {
-        val list = accountingDB.accountingDBQueries
-            .getGroupingMonthTotalAmount(yearAndMonth)
-            .executeAsList()
-            .map {
-              it.toGetGroupingMonthTotalAmountSerialization()
-            }
-        json.stringify(GetGroupingMonthTotalAmountSerialization.serializer().list, list)
-      }
+  }
+
+  suspend fun getGroupingMonthTotalAmountAsync(
+    yearAndMonth: String
+  ): Deferred<String> = coroutineScope {
+    return@coroutineScope async(runCoroutineDispatcher) {
+      val list = accountingDB.accountingDBQueries
+          .getGroupingMonthTotalAmount(yearAndMonth)
+          .executeAsList()
+          .map {
+            it.toGetGroupingMonthTotalAmountSerialization()
+          }
+      json.stringify(GetGroupingMonthTotalAmountSerialization.serializer().list, list)
     }
+  }
 }
