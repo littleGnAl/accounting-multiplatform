bcd8913d3b33decd11ce317ed0a8f369a33b2c65
littleGnAl <tearjeaker@hotmail.com>
2020-03-08 00:17:01 +0800

    Bump versions

--- ci/dartfmt_check.sh
+++ ci/dartfmt_check.sh
@@ -5,8 +5,8 @@ set -x
 output=$(dartfmt ./ -n)
 
 if [[ -z ${output} ]]
-  echo "The code style in ${output} is incorrect, make sure you follow the dart code style, please correct it by using dartfmt or manually."
   then exit 0
 else
+  echo "The code style in ${output} is incorrect, make sure you follow the dart code style, please correct it by using dartfmt or manually."
   exit 1
 fi
