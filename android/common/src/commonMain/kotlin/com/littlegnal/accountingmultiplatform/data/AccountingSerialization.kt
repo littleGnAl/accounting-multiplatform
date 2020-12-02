c18fb85a6ab590234d59e8e5e7c0ba83a643d52d
Littlegnal <8847263+littleGnAl@users.noreply.github.com>
2020-12-03 01:16:30 +0800

    Upgrade kotlin 1.4.20, flutter 1.22.4, bump libraries versions (#17)
    * Upgrade kotlin 1.4.20, flutter 1.22.4, bump libraries versions

--- android/common/src/commonMain/kotlin/com/littlegnal/accountingmultiplatform/data/AccountingSerialization.kt
+++ android/common/src/commonMain/kotlin/com/littlegnal/accountingmultiplatform/data/AccountingSerialization.kt
@@ -15,35 +15,17 @@
  */
 
 package com.littlegnal.accountingmultiplatform.data
-import kotlinx.serialization.Serializable
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.Serializer
 
-@Serializable
-data class AccountingSerialization(
-  override val id: Long,
-  override val amount: Double,
-  override val createTime: Long,
-  override val tag_name: String,
-  override val remarks: String?
-) : Accounting
+@ExperimentalSerializationApi
+@Serializer(forClass = Accounting::class)
+object AccountingSerialization
 
-@Serializable
-data class GetMonthTotalAmountSerialization(
-  override val total: Double?,
-  override val yearMonth: String?
-) : GetMonthTotalAmount
+@ExperimentalSerializationApi
+@Serializer(forClass = GetMonthTotalAmount::class)
+object GetMonthTotalAmountSerialization
 
-@Serializable
-data class GetGroupingMonthTotalAmountSerialization(
-  override val total: Double,
-  override val tag_name: String
-) : GetGroupingMonthTotalAmount
-
-fun Accounting.toAccountingSerialization(): AccountingSerialization =
-  AccountingSerialization(id, amount, createTime, tag_name, remarks)
-
-fun GetMonthTotalAmount.toGetMonthTotalAmountSerialization(): GetMonthTotalAmountSerialization =
-  GetMonthTotalAmountSerialization(total, yearMonth)
-
-fun GetGroupingMonthTotalAmount.toGetGroupingMonthTotalAmountSerialization():
-    GetGroupingMonthTotalAmountSerialization =
-  GetGroupingMonthTotalAmountSerialization(total, tag_name)
+@ExperimentalSerializationApi
+@Serializer(forClass = GetGroupingMonthTotalAmount::class)
+object GetGroupingMonthTotalAmountSerialization
